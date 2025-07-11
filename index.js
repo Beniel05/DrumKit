bb77b186706ddb3b79c05daa8ae4bd4ed996b922
beniel05 <benielebenezer@gmail.com>
2025-07-12 00:04:51 +0530

    Feat: added button animation after getting clicked or typed

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ for (var i = 0; i < buttonClicked.length; i++) {
     buttonClicked[i].addEventListener("click", function () {
         const key = this.innerHTML;
         makeSound(key);
+        buttonAnimation(key);
     });
 }
 
 document.addEventListener("keydown", function (event) {
     const keyPressed = event.key.toLowerCase();
     makeSound(keyPressed);
+    buttonAnimation(keyPressed);
 });
-
 
 
 function makeSound(key) {
@@ -39,4 +40,13 @@ function makeSound(key) {
         default:
             break;
     }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
