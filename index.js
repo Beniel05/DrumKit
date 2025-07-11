257cd4438bd1cd7933bbf8fa8fccdf72dd7912d1
beniel05 <benielebenezer@gmail.com>
2025-07-11 23:31:36 +0530

    Feat: Added index.js and Fixed index.html and style.css

---
+++ index.js
@@ -0,0 +1,42 @@
+const buttonClicked = document.querySelectorAll(".drum");
+for (var i = 0; i < buttonClicked.length; i++) {
+    buttonClicked[i].addEventListener("click", function () {
+        const key = this.innerHTML;
+        makeSound(key);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    const keyPressed = event.key.toLowerCase();
+    makeSound(keyPressed);
+});
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case "a":
+            new Audio("./assets/Sounds/tom-1.mp3").play();
+            break;
+        case "s":
+            new Audio("./assets/Sounds/tom-2.mp3").play();
+            break;
+        case "d":
+            new Audio("./assets/Sounds/tom-3.mp3").play();
+            break;
+        case "f":
+            new Audio("./assets/Sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("./assets/Sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("./assets/Sounds/kick-bass.mp3").play();
+            break;
+        case "l":
+            new Audio("./assets/Sounds/crash.mp3").play();
+            break;
+        default:
+            break;
+    }
+}
